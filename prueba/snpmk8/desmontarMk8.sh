3310298cf3271496e3cbf9187d9d53d3ce78f4ec
Manuel Ruiz <del-manu@hotmail.com>
2020-10-13 10:55:24 +0200

    ingress solved

--- prueba/snpmk8/desmontarMk8.sh
+++ prueba/snpmk8/desmontarMk8.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-mount | grep microk8s
+#mount | grep microk8s
 sudo umount /snap/microk8s/1668 
 sudo unsquashfs /var/lib/snapd/snaps/microk8s_1668.snap
--- prueba/snpmk8/montarMk8.sh
+++ prueba/snpmk8/montarMk8.sh
@@ -2,3 +2,5 @@
 sudo mksquashfs squashfs-root microk8s_1668.snap
 sudo cp microk8s_1668.snap /var/lib/snapd/snaps/microk8s_1668.snap
 sudo mount -t squashfs -o ro,nodev,relatime,x-gdu.hide /var/lib/snapd/snaps/microk8s_1668.snap /snap/microk8s/1668
+sudo rm -rf microk8s_1668.snap
+sudo rm -rf squashfs-root
