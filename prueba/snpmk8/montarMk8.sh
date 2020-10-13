3cb1cbf0a96b2c0ba8aea973915d7ec92dbeacf0
Manuel Ruiz <del-manu@hotmail.com>
2020-10-13 10:10:12 +0200

    laptop changes

---
+++ prueba/snpmk8/montarMk8.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo mksquashfs squashfs-root microk8s_1668.snap
+sudo cp microk8s_1668.snap /var/lib/snapd/snaps/microk8s_1668.snap
+sudo mount -t squashfs -o ro,nodev,relatime,x-gdu.hide /var/lib/snapd/snaps/microk8s_1668.snap /snap/microk8s/1668
