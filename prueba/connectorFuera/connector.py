d9cfce77636b841c45700003ee02159abc0ec1f7
roxax19 <del-manu@hotmail.com>
2020-10-16 12:10:02 +0200

    ended IoT devices authentication

--- prueba/connectorFuera/connector.py
+++ prueba/connectorFuera/connector.py
@@ -52,19 +52,16 @@ def on_message(client, userdata, msg):
     else:
       print('No se han introducido los datos correctamente')
 
-# Creamos las propieddes del cliente
+# Nos conectamos al mqtt broker
 client = mqtt.Client(client_id="Connector")
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Configuramos la conexion por TLS y con usr + pwd
 client.tls_set(ca_certs="ssl/myCA.pem", cert_reqs=mqtt.ssl.CERT_REQUIRED)
 client.username_pw_set("manuel", password="manuel")
+client.connect("broker", 8883, 60)
 
-# Nos conectamos con el broker
-client.connect("10.152.183.240", 1883, 60)
-
-# Nos conectamos a la base de datos
+#Nos conectamos a la base de datos
 mydb = mysql.connector.connect(
   host="10.152.183.137", #master
   port="3306",
--- prueba/snpmk8/desmontarMk8.sh
+++ prueba/snpmk8/desmontarMk8.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #HAY QUE MIRAR CUAL ES EL NUMERO DE NUESTRA INSTALACION
 #mount | grep microk8s
-sudo umount /snap/microk8s/1422 
-sudo unsquashfs /var/lib/snapd/snaps/microk8s_1422.snap
+sudo umount /snap/microk8s/1668
+sudo unsquashfs /var/lib/snapd/snaps/microk8s_1668.snap
