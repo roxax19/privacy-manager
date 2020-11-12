93980eddaa1b05ca6ccab110303990a0dbeb0585
Manuel Ruiz Ruiz <del-manu@hotmail.com>
2020-11-12 11:23:32 +0100

    added script for git

---
+++ gitscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+commitmsg=''
+
+while getopts 'm:' flag; do
+    case "${flag}" in
+        m) commitmsg="${OPTARG}" ;;
+    esac
+done
+
+git add .
+git commit -m "${commitmsg}"
+git push 
